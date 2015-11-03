7877c21cb1f1fc3c06c1d818ff017ace0c9d6262
Mark Curtis <mark.curtis@datastax.com>
2015-11-03 15:00:43 +0000

    Create iostat-service.sh

---
+++ iostat-service.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# iostat grapher. Note, in the following line the data
+# points will start at field $4 for rrqm/s
+#
+# 2015-10-29 16:31:08 Device:         rrqm/s   wrqm/s     r/s     w/s    rkB/s    wkB/s avgrq-sz avgqu-sz   await  svctm  %uti#
+# 2015-10-29 16:31:08 sda               0.00     2.60    0.00    3.40     0.00    22.40    13.18     0.00    0.18   0.18   0.06
+
+echo "usage: $0 <iostat.log> <disk name> <out.png>"
+echo "procesing $1 for device $2, plotting $3"
+
+cat $1 | grep "$2 " > dat.dat
+
+gnuplot <<_EOF_
+set terminal png
+set output "$3"
+set title "$2: Service time"
+set xdata time
+set timefmt "%Y-%m-%d %H:%M:%S"
+set format x "%H:%M\n%d/%m"
+set xtics nomirror scale 3,2
+set ylabel "Miliseconds"
+set samples 10
+plot "dat.dat" using 1:12 title "average wait time in ms (await)" with lines, \
+"dat.dat" using 1:13 title "average service time in ms (svctm)" with lines
+_EOF_
+
+rm dat.dat
