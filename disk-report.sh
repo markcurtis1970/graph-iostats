f08e217f0198557ff5866558bfec16238457bf80
markcurtis1970 <mark.curtis@datastax.com>
2016-04-25 16:25:39 +0100

    Add relative path to disk-report.sh

--- disk-report.sh
+++ disk-report.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
 # Loop through all discs to produce the iostat graphs
+CWD=$(dirname $0)
 
 if [ $# -ne 2 ];then
 	echo "Generate the iostat graphs from a list of discs"
@@ -15,10 +16,10 @@ IOSTATS=$2
 
 while read DISK
 do
-    ./iostat-readwrite-requests.sh $IOSTATS $DISK $DISK-requests.png
-    ./iostat-readwrite-throughput.sh $IOSTATS $DISK $DISK-throughput.png
-    ./iostat-queue-length.sh $IOSTATS $DISK $DISK-queue-length.png
-    ./iostat-queue-size.sh $IOSTATS $DISK $DISK-queue-size.png
-    ./iostat-service.sh $IOSTATS $DISK $DISK-service.png
-    ./iostat-utilisation.sh $IOSTATS $DISK $DISK-utilisation.png
+    $CWD/iostat-readwrite-requests.sh $IOSTATS $DISK $DISK-requests.png
+    $CWD/iostat-readwrite-throughput.sh $IOSTATS $DISK $DISK-throughput.png
+    $CWD/iostat-queue-length.sh $IOSTATS $DISK $DISK-queue-length.png
+    $CWD/iostat-queue-size.sh $IOSTATS $DISK $DISK-queue-size.png
+    $CWD/iostat-service.sh $IOSTATS $DISK $DISK-service.png
+    $CWD/iostat-utilisation.sh $IOSTATS $DISK $DISK-utilisation.png
 done < $DISKS
